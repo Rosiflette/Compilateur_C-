2a696cc6345328e9ff4a190c19c28cc3364eb64a
mirasl <mirasl@pc-dg-037-04.iut2.upmf-grenoble.fr>
2019-04-03 15:16:58 +0200

    Projet

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=intercompil
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/intercompil
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=intercompil.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/intercompil.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=intercompil
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/intercompil
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=intercompil.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/intercompil.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
